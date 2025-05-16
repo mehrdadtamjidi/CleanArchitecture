7e74b8df34b3fc8d6b89271fa4f780e50d237693
Mehrdad Tamjidi <mehrdad.tamjidi@gmail.com>
2025-05-16 22:19:28 +0330

    update

---
+++ CleanArchitecture.Infrastructure/Mail/EmailSender.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchitecture.Application.Contracts.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Infrastructure.Mail
+{
+    public class EmailSender : IEmailSender
+    {
+        public async Task<bool> SendEmail(string email)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
